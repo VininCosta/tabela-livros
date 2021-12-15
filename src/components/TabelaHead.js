372fbb4229315bc13fe403c728501b1a17488a3b
Vinicius Costa <viniciustvcosta@outlook.com>
2021-12-15 11:04:49 -0300

    Tabela

---
+++ src/components/TabelaHead.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const TabelaHead = (props) => (
+    <thead>
+        <tr>
+            <th colSpan="4">Tabela de livros</th>
+        </tr>
+        <tr>
+            <th>ISBN</th>
+            <th>Título
+                <div class="container-setas">
+                    <div onClick={ () => props.ordenarCrescente() }>&#129093;</div>
+                    <div onClick={ () => props.ordenarDecrescente() }>&#129095;</div>
+                </div>
+            </th>
+            <th>Autor</th>
+            <th></th>
+        </tr>
+    </thead>
+);
+
+export default TabelaHead;
